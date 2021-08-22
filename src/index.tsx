e62241fe62a490c3dce1707a8ee4020de9e567c7
kambleaa007 <ashishkamble191191@gmail.com>
2021-08-22 20:05:41 +0530

    adding world img

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,78 @@
-import ForgeUI, { render, Fragment, Macro, Text } from '@forge/ui';
+import api, { route } from "@forge/api";
+import ForgeUI, {
+  MacroConfig,
+  Image,
+  Macro,
+  Option,
+  render,
+  Select,
+  useAction,
+  useConfig,
+  useProductContext, Fragment, Text }
+from '@forge/ui';
+import { countryData, MapBuilder } from 'countries-svg/dist/es6';
 
 const App = () => {
 
-  console.log('Debuggin here')
+  const config = useConfig();
 
+  const addLabel = async () => {
+    if (config && config.selectedCountry) {
+      const context = useProductContext();
+      const bodyData = [{
+        prefix: "global",
+        name: config.selectedCountry
+      }];
+      await api
+        .asUser()
+        .requestConfluence(route`/wiki/rest/api/content/${context.contentId}/label`, {
+          method: 'POST',
+          body: JSON.stringify(bodyData)
+        });
+    }
+  };
+
+  useAction(value => value, async () => await addLabel());
+
+  const confluenceSectionWidth = 680;
+  const mapAspectRatio = 0.7;
+  const mapWidth = confluenceSectionWidth;
+  const mapHeight = mapAspectRatio * mapWidth;
+  let builder = new MapBuilder()
+    .setWidth(confluenceSectionWidth)
+    .setHeight(mapHeight)
+    .setNormalColor('#f2f2f2')
+    .setHighlightColor('#FF5630');
+  if (config && config.selectedCountry) {
+    builder = builder.addCountryToHighlight(config.selectedCountry);
+  }
+  const svg = builder.build();
   return (
+
     <Fragment>
-      <Text>Hello world!</Text>
+      <Text>Forgin Iron still in 21st century</Text>
+      <Text>Countries Map is here</Text>
+      <Image
+      src={`data:image/svg+xml;utf8,${encodeURIComponent(svg)}`}
+      alt='header'
+    />
     </Fragment>
+  );
+};
+
+const Config = () => {
+  const countryNames = countryData.getCountryNames();
+  const options = countryNames.map(countryName => {
+    return (
+      <Option label={countryName} value={countryName} />
+    );
+  });
+  return (
+    <MacroConfig>
+      <Select label="Countries" name="selectedCountry">
+        {options}
+      </Select>
+    </MacroConfig>
   );
 };
 
@@ -15,4 +80,7 @@ export const run = render(
   <Macro
     app={<App />}
   />
+);
+export const config = render(
+  <Config />
 );
